1c7f93c05a083555af28a766d398ebdac74c9ebb
Mário Ž <33102086+maarioz@users.noreply.github.com>
2021-02-27 23:46:33 +0100

    Update Snake.hpp

--- Snake.hpp
+++ Snake.hpp
@@ -10,19 +10,19 @@
 #include <ctime>
 
 namespace rng {
-  inline std::mt19937_64 mersenne{ static_cast<std::mt19937_64::result_type>(std::time(0)) };
+  inline std::mt19937_64 mersenne(static_cast<std::mt19937_64::result_type>(std::time(0)));
 } // namespace rng
 
 namespace constants {
-  inline constexpr int WIDTH{ 36 };
-  inline constexpr int HEIGHT{ 16 };
-  inline constexpr char SNAKE_SEGMENT_CHAR{ 'S' };
-  inline constexpr char FOOD_CHAR{ '*' };
+  inline constexpr int WIDTH = 36;
+  inline constexpr int HEIGHT = 16;
+  inline constexpr char SNAKE_SEGMENT_CHAR = 'S';
+  inline constexpr char FOOD_CHAR = '*';
 } // namespace constants
 
 struct SnakeSegment {
- int y{ 0 };
- int x{ 0 };
+ int y = 0;
+ int x = 0;
 
  SnakeSegment() = default;
  SnakeSegment(int y, int x) : y(y), x(x) {}
